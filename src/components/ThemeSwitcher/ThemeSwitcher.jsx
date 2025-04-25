d590c9cf4bd619d3a1905877e87389ff600570a6
Sasha Savenko <sashasavenko3@gmail.com>
2025-04-25 14:17:04 +0300

    toggler added

--- src/components/ThemeSwitcher/ThemeSwitcher.jsx
+++ src/components/ThemeSwitcher/ThemeSwitcher.jsx
@@ -1,24 +1,29 @@
-import { useEffect, useState } from 'react'
+// import { useEffect, useState } from 'react'
 import css from './ThemeSwitcher.module.css'
 import clsx from 'clsx'
 export default function ThemeSwitcher () {
-    const [theme, setTheme] = useState('light')
+    // const [theme, setTheme] = useState('light')
 
-    // useEffect(() => {
-    //     const savedTheme = localStorage.getItem("theme");
-    //     if (savedTheme) {
-    //       setTheme(savedTheme);
-    //       document.body.setAttribute("data-theme", savedTheme);
-    //     }
-    //   }, []);
+    // const handleTheme = (currentTheme) => {
+    //     console.log('theme switcher')
+    //     setTheme(currentTheme)
+    //     document.body.setAttribute('data-theme', theme)
+    // }
 
-    const handleTheme = (currentTheme) => {
-        console.log('theme switcher')
-        setTheme(currentTheme)
-        document.body.setAttribute('data-theme', theme)
+    const switchTheme = (e) => {
+if(e.target.checked){
+    document.querySelector('body').setAttribute('data-theme', 'dark')
+}else{
+    document.querySelector('body').setAttribute('data-theme', 'light')
+
+}
     }
     return <div className={css.themeBox}>
-        <div className={clsx(css.themeBtn, {[css.active]: theme === 'light'})} onClick={() => handleTheme('light')}></div>
-        <div className={clsx(css.themeBtn, {[css.active]: theme === 'dark'})} onClick={() => handleTheme('dark')}></div>
+        <label className={css.switch}>
+            <input type='checkbox' onChange={switchTheme}/>
+            <span className={clsx(css.slider, css.round) }></span>
+        </label>
+        {/* <div className={clsx(css.themeBtn, {[css.active]: theme === 'light'})} onClick={() => handleTheme('light')}></div>
+        <div className={clsx(css.themeBtn, {[css.active]: theme === 'dark'})} onClick={() => handleTheme('dark')}></div> */}
     </div>
 }
